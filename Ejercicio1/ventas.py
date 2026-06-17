ab780a62413aa06e235addfe9c5ad0bb99cc0078
Yuri Arbelo Gonzalez <yuri.arbelo85@gmail.com>
2026-06-17 09:45:28 -0500

    Modulo de Ventas sin persistencia aun

--- Ejercicio1/ventas.py
+++ Ejercicio1/ventas.py
@@ -1,11 +1,39 @@
+
 ventas = []
 
-def registrar_ventas():
+def find_productos(productos, nombre_producto):
+
+    for producto in productos:
+        if producto["nombre"].lower() == nombre_producto.lower():
+            return producto
+
+    return None
+
+
+def registrar_ventas(productos):
+
     fecha = input("Ingrese la fecha de la venta (dd/mm/yyyy): ")
-    producto = input("Ingrese el nombre del producto vendido: ")
+    nombre_producto = input("Ingrese el nombre del producto vendido: ")
+
+    producto = find_productos(productos, nombre_producto)
+
+    if producto is None:
+        print("Producto no encontrado.")
+        return
+
     cantidad = int(input("Ingrese la cantidad vendida: "))
-    precio_unitario = float(input("Ingrese el precio unitario del producto: "))
+
+    precio_unitario = producto["precio"]
     total_venta = cantidad * precio_unitario
 
-    print("Producto agregado exitosamente.")
+    venta = {
+        "fecha": fecha,
+        "producto": nombre_producto,
+        "cantidad": cantidad,
+        "precio_unitario": precio_unitario,
+        "total_venta": total_venta
+    }
 
+    ventas.append(venta)
+
+    print("Venta registrada correctamente.")
